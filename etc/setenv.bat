e9b38a2340496d58a79da14b5a2abb6c7028f2ac
lyly <lyly@d16f6d6b-91ff-4949-9588-15f209c5d49d>
2013-12-10 07:40:15 +0000

    add etc folder
    git-svn-id: https://192.168.50.100:443/svn/Tzuyu/trunk@3 d16f6d6b-91ff-4949-9588-15f209c5d49d

---
+++ etc/setenv.bat
@@ -0,0 +1,7 @@
+@echo on
+set BASE_PATH=D:\_1_Projects\Tzuyu
+set M2_HOME=%BASE_PATH%\tools\apache-maven-3.0.4
+set JAVA_HOME=%BASE_PATH%\tools\jdk1.6.0_26-64b
+
+set PATH=%M2_HOME%\bin;%JAVA_HOME%\bin;%PATH%
+
