73d0f857c6f242b777364106981c4a18b550d3c7
wang-jingyi <wangjy0721@hotmail.com>
2014-09-11 17:36:14 +0800

    uniform test case for demo MaxFindError

--- app/icsetlv/src/test/java/example/MaxFind/MaxFindError.java
+++ app/icsetlv/src/test/java/example/MaxFind/MaxFindError.java
@@ -1,50 +1,23 @@
 package example.MaxFind;
 
-import java.util.ArrayList;
 
 public class MaxFindError {
 
-	public static int findArrayMax(ArrayList<Integer> tlist) {
-		int max = tlist.get(0);
-		for (int i = 1; i < tlist.size() - 1; i++) {
-			if (max < tlist.get(i)) {
-				max = tlist.get(i);
+	public static int findArrayMax(int[] tlist) {
+		int max = tlist[0];
+		for (int i = 1; i < tlist.length - 1; i++) {
+			if (max < tlist[i]) {
+				max = tlist[i];
 			}
 		}
 		return max;
 	}
 	
-	public static boolean checkMax(ArrayList<Integer> tList, int max ){
+	public static boolean checkMax(int[] tList, int max ){
 		for(int i : tList){
 			assert max >= i : "Not right maximum number";
 			return true;
 		}
 		return false;
-	}
-	
-	public static int addup(int a , int b){
-		return a + b - 1 ;
-	}
-	
-//	public static int findSecondMax(ArrayList<Integer> tlist){
-//		int max1 = findArrayMax(tlist);
-//		tlist.remove(findIndex(tlist, max1));
-//		int max2 =findArrayMax(tlist);
-//		assert max2 < max1;
-//		return max2;
-//	}
-//	
-	
-	/*
-	 * pre-condition: elements in tlist is not equal to each other
-	 * 
-	 * */
-	public static int findIndex(ArrayList<Integer> tlist, int keyvalue){
-		for(int i = 0; i < tlist.size(); i++){
-			if(tlist.get(i) == keyvalue){
-				return i;
-			}
-		}
-		return 0;
 	}
 }
