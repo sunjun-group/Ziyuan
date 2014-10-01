a5d9a496ecddd2fa702a44ecc793a64773dd58b0
nguongphuc <nguongphuc@yahoo.com>
2014-10-01 16:36:29 +0800

    Support multiple ways to get the TRUNK value. I.e.: to be compatible with old structure.

--- app/sav.commons/src/test/java/commons/TestConfiguration.java
+++ app/sav.commons/src/test/java/commons/TestConfiguration.java
@@ -8,16 +8,20 @@
 
 package commons;
 
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
 import sav.common.core.utils.StringUtils;
 
 /**
  * @author LLT
- *
+ * 
  */
 public class TestConfiguration {
 	public static final String ICSETLV = "icsetlv";
 	public static final String FALTLOCALISATION = "faultLocalization";
-	
+	private static final String TZUYU_HOME = "TZUYU_HOME";
+
 	private static TestConfiguration config;
 	private static final String junitCore = "org.junit.runner.JUnitCore";
 	public String javaHome = System.getProperty("java.home");
@@ -25,21 +29,45 @@ public class TestConfiguration {
 	public String junitLib;
 	public String tracerLibPath;
 	public String javaSlicerPath;
-	
+
 	private TestConfiguration() {
-		TRUNK = System.getProperty("TZUYU_HOME");
+		TRUNK = getTrunkPath();
 		junitLib = TRUNK + "/app/icsetlv/src/test/lib/*";
 		tracerLibPath = TRUNK + "/etc/javaslicer/assembly/tracer.jar";
 	}
-	
+
+	/**
+	 * Try to get the trunk path from either:
+	 * <ol>
+	 * <li>System variable 'TZUYU_HOME'</li>
+	 * <li>Environment variable 'TZUYU_HOME'</li>
+	 * <li>Resource bundle 'test_configuration.properties'</li>
+	 * </ol>
+	 * If the value is defined in multiple locations, the first one is
+	 * preferred. If no value was defined in all three locations,
+	 * {@link MissingResourceException} is thrown.
+	 * 
+	 * @return path to trunk if defined in one of the three locations.
+	 */
+	private String getTrunkPath() {
+		String trunk = System.getProperty(TZUYU_HOME);
+		if (org.apache.commons.lang.StringUtils.isBlank(trunk)) {
+			trunk = System.getenv(TZUYU_HOME);
+			if (org.apache.commons.lang.StringUtils.isBlank(trunk)) {
+				trunk = ResourceBundle.getBundle("test_configuration").getString("trunk");
+			}
+		}
+		return trunk;
+	}
+
 	public String getTestScrPath(String module) {
 		return StringUtils.join("", TRUNK, "/app/", module, "/src/test/java");
 	}
-	
+
 	public String getTestTarget(String module) {
 		return StringUtils.join("", TRUNK, "/app/", module, "/target/test-classes");
 	}
-	
+
 	public String getTarget(String module) {
 		return StringUtils.join("", TRUNK, "/app/", module, "/target/classes");
 	}
@@ -54,7 +82,7 @@ public class TestConfiguration {
 	public String getJavahome() {
 		return javaHome;
 	}
-	
+
 	public String getJunitcore() {
 		return junitCore;
 	}
@@ -62,7 +90,7 @@ public class TestConfiguration {
 	public String getJavaBin() {
 		return getJavahome() + "/bin";
 	}
-	
+
 	public String getJunitLib() {
 		return junitLib;
 	}
