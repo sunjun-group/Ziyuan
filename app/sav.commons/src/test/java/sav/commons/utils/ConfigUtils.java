758f3af06ba81ae1c7074f84e3607a101bf01e28
nguongphuc <nguongphuc@yahoo.com>
2014-10-13 15:30:56 +0800

    Update dependency for Fitnesse test

--- app/sav.commons/src/test/java/sav/commons/utils/ConfigUtils.java
+++ app/sav.commons/src/test/java/sav/commons/utils/ConfigUtils.java
@@ -3,6 +3,8 @@ package sav.commons.utils;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
+import org.apache.commons.lang.StringUtils;
+
 public class ConfigUtils {
 	private static final String TZUYU_HOME = "TZUYU_HOME";
 
@@ -20,12 +22,9 @@ public class ConfigUtils {
 	 * @return path to trunk if defined in one of the three locations.
 	 */
 	public static String getTrunkPath() {
-		String trunk = System.getProperty(TZUYU_HOME);
-		if (org.apache.commons.lang.StringUtils.isBlank(trunk)) {
-			trunk = System.getenv(TZUYU_HOME);
-			if (org.apache.commons.lang.StringUtils.isBlank(trunk)) {
-				trunk = ResourceBundle.getBundle("test_configuration").getString("trunk");
-			}
+		String trunk = getProperty(TZUYU_HOME);
+		if (StringUtils.isBlank(trunk)) {
+			trunk = ResourceBundle.getBundle("test_configuration").getString("trunk");
 		}
 		return trunk;
 	}
@@ -37,5 +36,20 @@ public class ConfigUtils {
 	public static String getTracerLibPath() {
 		return getTrunkPath() + "/etc/javaslicer/assembly/tracer.jar";
 	}
-	
+
+	/**
+	 * Try to get the property identified with 'name' parameter by first check
+	 * in System Properties then Environment Properties.
+	 * 
+	 * @param name
+	 *            Name of the property
+	 * @return Value of the property, or <code>null</code> if nothing found
+	 */
+	public static String getProperty(final String name) {
+		String value = System.getProperty(name);
+		if (StringUtils.isBlank(value)) {
+			value = System.getenv(name);
+		}
+		return value;
+	}
 }
