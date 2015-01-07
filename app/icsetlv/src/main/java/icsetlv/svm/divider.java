fc1b3a062c9af57dc918ce67b03c01850906529e
nguongphuc <nguongphuc@yahoo.com>
2015-01-07 14:47:10 +0800

    Refactoring: simplify the codes + fix typo in test case

--- app/icsetlv/src/main/java/icsetlv/svm/divider.java
+++ app/icsetlv/src/main/java/icsetlv/svm/divider.java
@@ -3,25 +3,19 @@ package icsetlv.svm;
 public class divider {
 	double[] thetas;
 	double theta0;
-	
-	public divider(double[] tts, double tt0){
+
+	public divider(double[] tts, double tt0) {
 		this.thetas = tts;
 		this.theta0 = tt0;
 	}
-	
-	public String toString(){
-		int thlength = thetas.length;
+
+	public String toString() {
 		StringBuilder dsb = new StringBuilder();
-		dsb.append("[ ");
-		for(int i = 0; i < thlength; i++){
-			if(i == thlength - 1){
-				dsb.append(thetas[i] +", " + theta0);
-			}
-			else{
-				dsb.append(thetas[i] + "  ");
-			}
+		dsb.append("[");
+		for (double theta : thetas) {
+			dsb.append("  ").append(theta);
 		}
-		dsb.append("; ]");
+		dsb.append(", ").append(theta0).append("; ]");
 		return dsb.toString();
 	}
 }
