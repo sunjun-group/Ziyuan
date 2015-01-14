271cc911868621b4b7f50ac4303830a76d5e0d8a
nguongphuc <nguongphuc@yahoo.com>
2015-01-14 13:11:30 +0800

    Porting functions from Jingyi's LibSVM to the new component

--- app/svmlib/src/main/java/libsvm/core/Machine.java
+++ app/svmlib/src/main/java/libsvm/core/Machine.java
@@ -157,6 +157,101 @@ public class Machine {
 		}
 	}
 
+	public double[] getWeights() {
+		Assert.assertNotNull("SVM parameters is not set.", parameter);
+		Assert.assertNotNull("SVM model is not available yet.", model);
+
+		// Weights are only available for linear SVMs
+		if (parameter.svm_type != svm_parameter.C_SVC) {
+			return null;
+		}
+
+		final int numberOfFeatures = data.get(0).getNumberOfFeatures();
+
+		double[][] prob = new double[model.SV.length][numberOfFeatures];
+		for (int i = 0; i < model.SV.length; i++) {
+			for (int j = 0; j < numberOfFeatures; j++) {
+				prob[i][j] = 0;
+			}
+		}
+		for (int i = 0; i < model.SV.length; i++) {
+			for (int j = 0; j < model.SV[i].length; j++) {
+				prob[i][model.SV[i][j].index] = model.SV[i][j].value;
+			}
+		}
+
+		double w_list[][][] = new double[model.nr_class][model.nr_class - 1][numberOfFeatures];
+
+		for (int i = 0; i < numberOfFeatures; ++i) {
+			for (int j = 0; j < model.nr_class - 1; ++j) {
+				int index = 0;
+				int end = 0;
+				double acc;
+				for (int k = 0; k < model.nr_class; ++k) {
+					acc = 0.0;
+					index += (k == 0) ? 0 : model.nSV[k - 1];
+					end = index + model.nSV[k];
+					for (int m = index; m < end; ++m) {
+						acc += model.sv_coef[j][m] * prob[m][i];
+					}
+					w_list[k][j][i] = acc;
+				}
+			}
+		}
+
+		double[] weights = new double[numberOfFeatures];
+		for (int i = 0; i < model.nr_class - 1; ++i) {
+			for (int j = i + 1, k = i; j < model.nr_class; ++j, ++k) {
+				for (int m = 0; m < numberOfFeatures; ++m) {
+					weights[m] = (w_list[i][k][m] + w_list[j][i][m]);
+
+				}
+			}
+		}
+		return weights;
+	}
+
+	public Divider getExplicitDivider() {
+		Assert.assertTrue("SVM training data is empty.", !data.isEmpty());
+		Assert.assertNotNull("SVM model is not available yet.", model);
+
+		int pSize = data.get(0).getNumberOfFeatures();
+		double bias = 0;
+		double[] wVec = new double[pSize];
+
+		int[] sIndex = new int[model.nr_class];
+		for (int i = 1; i < model.nr_class; i++) {
+			sIndex[i] = sIndex[i - 1] + model.nSV[i - 1];
+		}
+
+		int p = 0;
+		for (int i = 0; i < model.nr_class; i++) {
+			for (int j = i + 1; j < model.nr_class; j++) {
+				int si = sIndex[i];
+				int sj = sIndex[j];
+				double[] coef1 = model.sv_coef[j - 1];
+				double[] coef2 = model.sv_coef[i];
+
+				for (int k = 0; k < model.nSV[i]; k++) {
+					for (int m = 0; m < pSize; m++) {
+						wVec[m] += coef1[si + k] * model.SV[si + k][m].value;
+					}
+				}
+
+				for (int k = 0; k < model.nSV[j]; k++) {
+					for (int m = 0; m < pSize; m++) {
+						wVec[m] += coef2[sj + k] * model.SV[sj + k][m].value;
+					}
+				}
+
+				bias = model.rho[p];
+				p++;
+			}
+		}
+
+		return new Divider(wVec, bias);
+	}
+
 	public double getModelAccuracy() {
 		Assert.assertNotNull("SVM model is not available yet.", model);
 		int rightClassification = 0, wrongClassification = 0;
