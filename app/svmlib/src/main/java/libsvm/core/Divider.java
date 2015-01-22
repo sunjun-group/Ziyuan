e73b1666f697db031df83fa90269ec4e9177e43c
nguongphuc <nguongphuc@yahoo.com>
2015-01-22 13:09:11 +0800

    Code enhancement: use JBLAS for matrix operations for clearer codes and faster calculation (as JBLAS uses multithreaded ATLAS)

--- app/svmlib/src/main/java/libsvm/core/Divider.java
+++ app/svmlib/src/main/java/libsvm/core/Divider.java
@@ -1,5 +1,13 @@
 package libsvm.core;
 
+/**
+ * Represents a way to divide a set of {@link DataPoint} into two separated sets
+ * with two different characteristics. It is a 'line' specified by: <br/>
+ * thetas[0]*x0 + thetas[1]*x1 + ... + thetas[n]*xn = theta0
+ * 
+ * @author Nguyen Phuoc Nguong Phuc (npn)
+ * 
+ */
 public class Divider {
 	private final double[] thetas;
 	private final double theta0;
@@ -10,12 +18,14 @@ public class Divider {
 	}
 
 	public String toString() {
+		// If the function is a*x0 + b*x1 + c*x2 = d
+		// Then the string representation will be [a b c : d]
 		final StringBuilder dsb = new StringBuilder();
 		dsb.append("[");
 		for (double theta : thetas) {
 			dsb.append("  ").append(theta);
 		}
-		dsb.append(", ").append(theta0).append("; ]");
+		dsb.append(" : ").append(theta0).append("]");
 		return dsb.toString();
 	}
 }
--- app/svmlib/src/main/java/libsvm/core/Model.java
+++ app/svmlib/src/main/java/libsvm/core/Model.java
@@ -1,19 +1,149 @@
 package libsvm.core;
 
 import libsvm.svm_model;
+import libsvm.svm_node;
+import libsvm.svm_parameter;
+
+import org.jblas.DoubleMatrix;
+import org.junit.Assert;
 
 /**
- * Wrapper for libsvm.svm_model
+ * This class represents the result of a learning process
  * 
  * @author Nguyen Phuoc Nguong Phuc (npn)
  * 
  */
 public class Model {
 
-	private svm_model model;
+	private static final int NUMBER_OF_CLASSES_IN_BINARY_MODEL = 2;
 
-	public Model(final svm_model model) {
+	private final svm_model model;
+	private final int numberOfFeatures;
+
+	public Model(final svm_model model, final int numberOfFeatures) {
+		Assert.assertNotNull("SVM model cannot be null", model);
 		this.model = model;
+		this.numberOfFeatures = numberOfFeatures;
+	}
+
+	public int getNumberOfClasses() {
+		return model.nr_class;
+	}
+
+	public boolean isBinary() {
+		return NUMBER_OF_CLASSES_IN_BINARY_MODEL == getNumberOfClasses();
+	}
+
+	public int getNumberOfSupportVectors() {
+		return model.l;
+	}
+
+	public double[] getWeights() {
+		Assert.assertNotNull("SVM model is not available yet.", model);
+
+		// Weights are only available for linear SVMs
+		if (model.param.svm_type != svm_parameter.C_SVC) {
+			return null;
+		}
+
+		double[][] prob = new double[model.SV.length][numberOfFeatures];
+		for (int i = 0; i < model.SV.length; i++) {
+			for (int j = 0; j < numberOfFeatures; j++) {
+				prob[i][j] = 0;
+			}
+		}
+		for (int i = 0; i < model.SV.length; i++) {
+			for (int j = 0; j < model.SV[i].length; j++) {
+				prob[i][model.SV[i][j].index] = model.SV[i][j].value;
+			}
+		}
+
+		double w_list[][][] = new double[model.nr_class][model.nr_class - 1][numberOfFeatures];
+
+		for (int i = 0; i < numberOfFeatures; ++i) {
+			for (int j = 0; j < model.nr_class - 1; ++j) {
+				int index = 0;
+				int end = 0;
+				double acc;
+				for (int k = 0; k < model.nr_class; ++k) {
+					acc = 0.0;
+					index += (k == 0) ? 0 : model.nSV[k - 1];
+					end = index + model.nSV[k];
+					for (int m = index; m < end; ++m) {
+						acc += model.sv_coef[j][m] * prob[m][i];
+					}
+					w_list[k][j][i] = acc;
+				}
+			}
+		}
+
+		double[] weights = new double[numberOfFeatures];
+		for (int i = 0; i < model.nr_class - 1; ++i) {
+			for (int j = i + 1, k = i; j < model.nr_class; ++j, ++k) {
+				for (int m = 0; m < numberOfFeatures; ++m) {
+					weights[m] = (w_list[i][k][m] + w_list[j][i][m]);
+
+				}
+			}
+		}
+		return weights;
+	}
+
+	/**
+	 * Get the {@link Divider} for this {@link Model}.<br/>
+	 * The linear function is <code>wT * x + b = 0</code>. <br/>
+	 * In which:
+	 * <ul>
+	 * <li>the weight matrix <code>W = model.SV * model.coef</code></li>
+	 * <li><code>wT</code> is the transition of <code>W</code></li>
+	 * <li>the bias matrix<code>b = mode.rho</code></li>
+	 * </ul>
+	 * We assume that the bias matrix contains only 1 element. I.e.: double[1].
+	 * 
+	 * @return The divider which can be use to categorize the learned data.
+	 */
+	public Divider getExplicitDivider() {
+		Assert.assertNotNull("SVM model is not available yet.", model);
+
+		// coef = [x][number of SVs]
+		DoubleMatrix coefficientMatrix = new DoubleMatrix(model.sv_coef);
+		// (!) NOTE: We assert that x is always equal to 1
+		Assert.assertTrue("Unexpected size of matrices.", coefficientMatrix.getRows() == 1);
+
+		double[][] supportVectors = new double[model.SV.length][];
+		{
+			int i = 0;
+			for (svm_node[] row : model.SV) {
+				supportVectors[i] = new double[row.length];
+				int j = 0;
+				for (svm_node node : row) {
+					supportVectors[i][j++] = node.value;
+				}
+				i++;
+			}
+		}
+
+		// sv = [number of SVs][number of features]
+		DoubleMatrix supportVectorMatrix = new DoubleMatrix(supportVectors);
+
+		Assert.assertTrue("Cannot multiply coefficient matrix [" + coefficientMatrix.rows + "]["
+				+ coefficientMatrix.columns + "]" + " with support vector matrix ["
+				+ supportVectorMatrix.rows + "][" + supportVectorMatrix.columns + "].",
+				coefficientMatrix.multipliesWith(supportVectorMatrix));
+
+		// w = [x][number of features]
+		// wT = [number of features][x]
+		// (!) NOTE: we don't explicitly transpose the matrix
+		final DoubleMatrix weightMatrix = coefficientMatrix.mmul(supportVectorMatrix);
+
+		double[] result = new double[weightMatrix.getColumns()];
+		for (int i = 0; i < weightMatrix.getColumns(); i++) {
+			result[i] = weightMatrix.get(0, i);
+		}
+
+		// b = [x] = [model.rho]
+		// The function is wT * x + b = 0
+		return new Divider(result, model.rho[0]);
 	}
 
 }
