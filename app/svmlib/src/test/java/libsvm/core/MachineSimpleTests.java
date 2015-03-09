fd53bbaaf9bec887d211260808f38cc14b2bb000
truongkhanhit <truongkhanhit@gmail.com>
2015-03-09 17:40:36 +0800

    refactor Test

--- app/svmlib/src/test/java/libsvm/core/MachineSimpleTests.java
+++ app/svmlib/src/test/java/libsvm/core/MachineSimpleTests.java
@@ -1,93 +1,26 @@
 package libsvm.core;
 
 import java.io.InputStream;
-import java.util.Scanner;
-import org.apache.log4j.Level;
 import org.junit.Test;
 
 public class MachineSimpleTests extends TestUltility{
 
-	private Machine normalMachine;
-	
 	@Test
 	public void whenThereAreTwoFeatures() {
-		normalMachine = setupMachine(new Machine(), 2);
-		
-		// Separator: ax + by = c
-		int x, y;
 		InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("TwoFeatures.txt");
-		Scanner scanner = new Scanner(inputStream);
-		int numberOfPositivePoints = scanner.nextInt();
-		
-		for (int i = 0; i < numberOfPositivePoints; i++) {
-			Category category = Category.POSITIVE;
-			x = scanner.nextInt();
-			y = scanner.nextInt();
-			normalMachine.addDataPoint(category, x, y);
-		}
-
-		int numberOfNegativePoints = scanner.nextInt();
-		for (int i = 0; i < numberOfNegativePoints; i++) {
-			Category category = Category.NEGATIVE;
-			x = scanner.nextInt();
-			y = scanner.nextInt();
-			normalMachine.addDataPoint(category, x, y);
-		}
-
-		final double normalModelAccuracy = normalMachine.train().getModelAccuracy();
-		LOGGER.log(Level.DEBUG, "Normal SVM:" + normalModelAccuracy);
-		
-		LOGGER.info("Learned logic:");
-		LOGGER.info("Normal machine:\n" + normalMachine.getLearnedLogic());
-		
-		Divider divider = normalMachine.getModel().getExplicitDivider();
-		double[] expectedCoefficients = new double[]{-2, -3, -15};
-		
-		double[] coefficients = new CoefficientProcessing().process(divider);
-		compareCoefficients(expectedCoefficients, coefficients);
+		runTest(new Machine(), 2, new double[]{-2, -3, -15}, inputStream);
 	}
 
 	@Test
 	public void whenThereAreThreeFeatures() {
-		normalMachine = setupMachine(new Machine(), 3);
-		
-		// Separator: ax + by + cz = d
-		int x, y, z;
-		
 		InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("ThreeFeatures.txt");
-		Scanner scanner = new Scanner(inputStream);
-		int numberOfPositivePoints = scanner.nextInt();
-		
-		for (int i = 0; i < numberOfPositivePoints; i++) {
-			Category category = Category.POSITIVE;
-			x = scanner.nextInt();
-			y = scanner.nextInt();
-			z = scanner.nextInt();
-
-			normalMachine.addDataPoint(category, x, y, z);
-		}
-		
-		int numberOfNegativePoints = scanner.nextInt();
-		for (int i = 0; i < numberOfNegativePoints; i++) {
-			Category category = Category.NEGATIVE;
-			x = scanner.nextInt();
-			y = scanner.nextInt();
-			z = scanner.nextInt();
-
-			normalMachine.addDataPoint(category, x, y, z);
-		}
-
-		final double normalModelAccuracy = normalMachine.train().getModelAccuracy();
-		LOGGER.log(Level.DEBUG, "Normal SVM:" + normalModelAccuracy);
-		
-		LOGGER.info("Learned logic:");
-		LOGGER.info("Normal machine:\n" + normalMachine.getLearnedLogic());		
-		
-		Divider divider = normalMachine.getModel().getExplicitDivider();
-		double[] expectedCoefficients = new double[]{3, 7, 19, 80};
-		
-		double[] coefficients = new CoefficientProcessing().process(divider);
-		compareCoefficients(expectedCoefficients, coefficients);
+		runTest(new Machine(), 3, new double[]{3, 7, 19, 80}, inputStream);
+	}
+	
+	@Test
+	public void whenRequireManyDividersInPositiveMachine() {
+		InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("ManyDuplicatedCuts.txt");
+		runTest(new Machine(), 3, new double[]{3, 7, 19, 80}, inputStream);
 	}
 	
 //	@Test
@@ -96,11 +29,11 @@ public class MachineSimpleTests extends TestUltility{
 //		for (int i = 0; i < NUMBER_OF_DATA_POINTS; i++) {
 //			final double[] values = { Math.random(), Math.random() };
 //			final Category category = Category.random();
-//			normalMachine.addDataPoint(category, values);
+//			machine.addDataPoint(category, values);
 //			improvedMachine.addDataPoint(category, values);
 //		}
 //
-//		final double normalModelAccuracy = normalMachine.train().getModelAccuracy();
+//		final double normalModelAccuracy = machine.train().getModelAccuracy();
 //		LOGGER.log(Level.DEBUG, "Normal SVM:" + normalModelAccuracy);
 //		final double improvedModelAccuracy = improvedMachine.train().getModelAccuracy();
 //		LOGGER.log(Level.DEBUG, "Improved SVM:" + improvedModelAccuracy);
@@ -109,7 +42,7 @@ public class MachineSimpleTests extends TestUltility{
 //				Double.compare(normalModelAccuracy, improvedModelAccuracy) <= 0);
 //		
 //		LOGGER.info("Learned logic:");
-//		LOGGER.info("Normal machine: " + normalMachine.getLearnedLogic());
+//		LOGGER.info("Normal machine: " + machine.getLearnedLogic());
 //		LOGGER.info("Improved machine: " + improvedMachine.getLearnedLogic());
 //	}
 //	
@@ -128,14 +61,14 @@ public class MachineSimpleTests extends TestUltility{
 //			} else {
 //				countNegative++;
 //			}
-//			normalMachine.addDataPoint(category, x, y);
+//			machine.addDataPoint(category, x, y);
 //			positiveSeparationMachine.addDataPoint(category, x, y);
 //		}
 //
 //		LOGGER.log(Level.DEBUG, "Positive cases =" + countPositive);
 //		LOGGER.log(Level.DEBUG, "Negative cases =" + countNegative);
 //
-//		final double normalModelAccuracy = normalMachine.train().getModelAccuracy();
+//		final double normalModelAccuracy = machine.train().getModelAccuracy();
 //		LOGGER.log(Level.DEBUG, "Normal SVM:" + normalModelAccuracy);
 //		final double improvedModelAccuracy = positiveSeparationMachine.train().getModelAccuracy();
 //		LOGGER.log(Level.DEBUG, "Improved SVM:" + improvedModelAccuracy);
@@ -147,7 +80,7 @@ public class MachineSimpleTests extends TestUltility{
 //				Double.compare(normalModelAccuracy, improvedModelAccuracy) < 0);
 //
 //		LOGGER.info("Learned logic:");
-//		LOGGER.info("Normal machine: " + normalMachine.getLearnedLogic());
+//		LOGGER.info("Normal machine: " + machine.getLearnedLogic());
 //		LOGGER.info("PS machine: " + positiveSeparationMachine.getLearnedLogic());
 //	}
 	
--- app/svmlib/src/test/java/libsvm/core/TestUltility.java
+++ app/svmlib/src/test/java/libsvm/core/TestUltility.java
@@ -8,6 +8,10 @@
 
 package libsvm.core;
 
+import java.io.InputStream;
+import java.util.Scanner;
+
+import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.junit.Assert;
 
@@ -19,20 +23,70 @@ public class TestUltility {
 
 	protected static final Logger LOGGER = Logger.getLogger(MachineSimpleTests.class);
 
-	protected static final int NUMBER_OF_FEATURES = 2;
-	protected static final int NUMBER_OF_DATA_POINTS = 1000;
+	protected Machine machine;
 	
-	protected Machine setupMachine(final Machine machine, int numberOfFeatures) {
-		return machine.setNumberOfFeatures(numberOfFeatures).setParameter(
+	protected Machine setupMachine(Machine defaultMachine, int numberOfFeatures) {
+		return defaultMachine.setNumberOfFeatures(numberOfFeatures).setParameter(
 				new Parameter().setMachineType(MachineType.C_SVC).setKernelType(KernelType.LINEAR)
 						.setEps(1.0).setUseShrinking(false).setPredictProbability(false));
 	}
 	
+	protected void runTest(Machine defaultMachine, int numOfFeatures, double[] expectedCoefficients, InputStream inputStream) {
+		machine = setupMachine(defaultMachine, numOfFeatures);
+		
+		readDataFromFile(inputStream, numOfFeatures);
+
+		final double normalModelAccuracy = machine.train().getModelAccuracy();
+		log(normalModelAccuracy);
+		
+		checkLastDivider(expectedCoefficients);
+	}
+
+	
+	
+	private void readDataFromFile(InputStream inputStream, int numberOfFeatures){
+		Scanner scanner = new Scanner(inputStream);
+		readDataOfCategory(scanner, numberOfFeatures, Category.POSITIVE);
+		readDataOfCategory(scanner, numberOfFeatures, Category.NEGATIVE);
+	}
+
+	private void readDataOfCategory(Scanner scanner, int numberOfFeatures,
+			Category category) {
+		int numberOfPositivePoints = scanner.nextInt();
+		for (int i = 0; i < numberOfPositivePoints; i++) {
+			double [] data = readIntegerArray(scanner, numberOfFeatures);
+			machine.addDataPoint(category, data);
+		}
+	}
+	
+	private double[] readIntegerArray(Scanner scanner, int length){
+		double[] result = new double[length];
+		for(int i = 0; i < length; i++){
+			result[i] = scanner.nextInt();
+		}
+		
+		return result;
+	}
+	
+	private void log(final double normalModelAccuracy) {
+		LOGGER.log(Level.DEBUG, "Normal SVM:" + normalModelAccuracy);
+		
+		LOGGER.info("Learned logic:");
+		LOGGER.info("Normal machine:\n" + machine.getLearnedLogic());
+	}
+	
+	private void checkLastDivider(double[] expectedCoefficients) {
+		Divider divider = machine.getModel().getExplicitDivider();
+		double[] coefficients = new CoefficientProcessing().process(divider);
+		compareCoefficients(expectedCoefficients, coefficients);
+	}
+
 	/**
-	 * @param expectedCoefficients
-	 * @param coefficients
+	 * Compare 2 arrays, each pair has small different in EPSILON rate
 	 */
-	protected void compareCoefficients(double[] expectedCoefficients, double[] coefficients) {
+	private void compareCoefficients(double[] expectedCoefficients, double[] coefficients) {
+		Assert.assertEquals(expectedCoefficients.length, coefficients.length);
+		
 		double EPSILON = 0.2;
 		for(int i = 0; i < coefficients.length; i++){
 			double errorRate = Math.abs((coefficients[i] - expectedCoefficients[i]) / expectedCoefficients[i]);
