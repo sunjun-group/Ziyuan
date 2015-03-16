91d8cf23f377788b096311ff06745923e98c29f2
truongkhanhit <truongkhanhit@gmail.com>
2015-03-16 16:49:38 +0800

    extend drawLine.m

--- app/svmlib/src/test/resources/drawLine.m
+++ app/svmlib/src/test/resources/drawLine.m
@@ -1,8 +1,19 @@
-function [ output_args ] = drawLine( a, b, c, minX, maxX )
+function [ output_args ] = drawLine( a, b, c, minX, maxX, minY, maxY)
 %Draw line ax + by = c where minX <= x <= maxX
-x = linspace(minX, maxX);
-y = (c - a*x) / b;
-output_args = plot(x, y);
 
+if(a == 0)
+    y = c/b;
+    output_args = plot([minX, maxX], [y, y]);
+else
+    if (b == 0)
+        x = c/a;
+        output_args = plot([x, x], [minY, maxY]);
+    else
+        x = [minX, maxX];
+        y = (c - a*x) / b;
+        output_args = plot(x, y);
+    end
+end
+axis([minX, maxX, minY, maxY]);
 end
 
