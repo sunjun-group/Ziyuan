e985afeab6b2f818c7363850a1f076be2a8150dc
lylytran <lylypp6987@gmail.com>
2015-06-04 09:20:30 +0800

    project clean up

--- etc/install-javaslicer.bat
+++ etc/install-javaslicer.bat
@@ -10,3 +10,12 @@ call mvn install:install-file -Dfile=%JARS_PATH%/tracer.jar -Dsources=%SRC_PATH%
 call mvn install:install-file -Dfile=%JARS_PATH%/traceReader.jar -Dsources=%SRC_PATH%/javaslicer_src_20101004.zip -DgroupId=de.unisb.cs.st -DartifactId=traceReader -Dversion=20101004 -Dpackaging=jar
 
 call mvn install:install-file -Dfile=%JARS_PATH%/visualize.jar -Dsources=%SRC_PATH%/javaslicer_src_20101004.zip -DgroupId=de.unisb.cs.st -DartifactId=visualize -Dversion=20101004 -Dpackaging=jar
+
+rem THIS PART IS FOR DEPLOYING TO NEXUS SERVER
+rem call mvn deploy:deploy-file -Dfile=%JARS_PATH%/slicer.jar -Dsources=%SRC_PATH%/javaslicer_src_20101004.zip -DgroupId=de.unisb.cs.st -DartifactId=slicer -Dversion=20101004 -Dpackaging=jar -DrepositoryId=tzuyu-thirdparty -Durl=http://202.94.70.100:8081/nexus/content/repositories/thirdparty
+
+rem call mvn deploy:deploy-file -Dfile=%JARS_PATH%/tracer.jar -Dsources=%SRC_PATH%/javaslicer_src_20101004.zip -DgroupId=de.unisb.cs.st -DartifactId=tracer -Dversion=20101004 -Dpackaging=jar -DrepositoryId=tzuyu-thirdparty -Durl=http://202.94.70.100:8081/nexus/content/repositories/thirdparty
+
+rem call mvn deploy:deploy-file -Dfile=%JARS_PATH%/traceReader.jar -Dsources=%SRC_PATH%/javaslicer_src_20101004.zip -DgroupId=de.unisb.cs.st -DartifactId=traceReader -Dversion=20101004 -Dpackaging=jar -DrepositoryId=tzuyu-thirdparty -Durl=http://202.94.70.100:8081/nexus/content/repositories/thirdparty
+
+rem call mvn deploy:deploy-file -Dfile=%JARS_PATH%/visualize.jar -Dsources=%SRC_PATH%/javaslicer_src_20101004.zip -DgroupId=de.unisb.cs.st -DartifactId=visualize -Dversion=20101004 -Dpackaging=jar -DrepositoryId=tzuyu-thirdparty -Durl=http://202.94.70.100:8081/nexus/content/repositories/thirdparty
