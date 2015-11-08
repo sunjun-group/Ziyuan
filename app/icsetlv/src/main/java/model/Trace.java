6562147e06d58304b89530511a26cb99440ddfb3
Yun Lin <llmhyy@gmail.com>
2015-11-08 21:41:49 +0800

    build some data structure

---
+++ app/icsetlv/src/main/java/model/Trace.java
@@ -0,0 +1,23 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class stands for a trace for an execution
+ * @author "linyun"
+ *
+ */
+public class Trace {
+	private List<TraceNode> exectionList = new ArrayList<>();
+
+	public List<TraceNode> getExectionList() {
+		return exectionList;
+	}
+
+	public void setExectionList(List<TraceNode> exectionList) {
+		this.exectionList = exectionList;
+	}
+	
+	
+}
---
+++ app/icsetlv/src/main/java/model/TraceNode.java
@@ -0,0 +1,34 @@
+package model;
+
+public class TraceNode {
+	private String className;
+	private int lineNum;
+	
+	private ProgramState state;
+
+	public String getClassName() {
+		return className;
+	}
+
+	public void setClassName(String className) {
+		this.className = className;
+	}
+
+	public int getLineNum() {
+		return lineNum;
+	}
+
+	public void setLineNum(int lineNum) {
+		this.lineNum = lineNum;
+	}
+
+	public ProgramState getState() {
+		return state;
+	}
+
+	public void setState(ProgramState state) {
+		this.state = state;
+	}
+	
+	
+}
