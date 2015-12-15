fcf8dc1196901cff99698748bf0b8a034f7352ad
Yun Lin <llmhyy@gmail.com>
2015-12-15 20:03:39 +0800

    Slightly change the waiting time of the asynchronizing returning focus back to search text.

--- microbat/src/main/microbat/views/TraceView.java
+++ microbat/src/main/microbat/views/TraceView.java
@@ -193,7 +193,7 @@ public class TraceView extends ViewPart {
 									@Override
 									public void run() {
 										try {
-											Thread.sleep(1000);
+											Thread.sleep(300);
 										} catch (InterruptedException e) {
 											e.printStackTrace();
 										}
