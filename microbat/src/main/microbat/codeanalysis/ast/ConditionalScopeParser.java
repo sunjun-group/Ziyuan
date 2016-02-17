f7a8372124b6773396a343e1b89958cac13bb657
Yun Lin <llmhyy@gmail.com>
2016-02-17 16:28:20 +0800

    parse the scope of conditional branch, ready for building control dependency of trace model.

---
+++ microbat/src/main/microbat/codeanalysis/ast/ConditionalScopeParser.java
@@ -0,0 +1,100 @@
+package microbat.codeanalysis.ast;
+
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.DoStatement;
+import org.eclipse.jdt.core.dom.EnhancedForStatement;
+import org.eclipse.jdt.core.dom.ForStatement;
+import org.eclipse.jdt.core.dom.IfStatement;
+import org.eclipse.jdt.core.dom.Statement;
+import org.eclipse.jdt.core.dom.SwitchStatement;
+import org.eclipse.jdt.core.dom.WhileStatement;
+
+public class ConditionalScopeParser {
+	class Scope{
+		CompilationUnit cu;
+		int startLine;
+		int endLine;
+	}
+	
+	class ScopeVisitor extends ASTVisitor{
+		CompilationUnit cu;
+		Scope scope;
+		int conditionLineNumber;
+		
+		public ScopeVisitor(CompilationUnit cu, int lineNumber){
+			this.cu = cu;
+			this.conditionLineNumber = lineNumber;
+		}
+		
+		public boolean visit(DoStatement statement){
+			int line = cu.getLineNumber(statement.getExpression().getStartPosition());
+			if(line == conditionLineNumber){
+				setScope(statement);
+				return false;
+			}
+			
+			return true;
+		}
+		
+		public boolean visit(EnhancedForStatement statement){
+			int line = cu.getLineNumber(statement.getStartPosition());
+			if(line == conditionLineNumber){
+				setScope(statement);
+				return false;
+			}
+			return true;
+		}
+		
+		public boolean visit(ForStatement statement){
+			int line = cu.getLineNumber(statement.getExpression().getStartPosition());
+			if(line == conditionLineNumber){
+				setScope(statement);
+				return false;
+			}
+			return true;
+		}
+		
+		public boolean visit(IfStatement statement){
+			int line = cu.getLineNumber(statement.getExpression().getStartPosition());
+			if(line == conditionLineNumber){
+				setScope(statement);
+				return false;
+			}
+			return true;
+		}
+		
+		public boolean visit(SwitchStatement statement){
+			int line = cu.getLineNumber(statement.getExpression().getStartPosition());
+			if(line == conditionLineNumber){
+				setScope(statement);
+				return false;
+			}
+			return true;
+		}
+		
+		public boolean visit(WhileStatement statement){
+			int line = cu.getLineNumber(statement.getExpression().getStartPosition());
+			if(line == conditionLineNumber){
+				setScope(statement);
+				return false;
+			}
+			return true;
+		}
+		
+		private void setScope(Statement statement){
+			scope.cu = cu;
+			scope.startLine = cu.getLineNumber(statement.getStartPosition());
+			scope.endLine = cu.getLineNumber(statement.getStartPosition()+statement.getLength());
+		}
+	}
+	
+	public Scope parseScope(CompilationUnit cu, int conditionLineNumber){
+		
+		ScopeVisitor scopeVisitor = new ScopeVisitor(cu, conditionLineNumber);
+		cu.accept(scopeVisitor);
+		
+		Scope scope = scopeVisitor.scope;
+		return scope;
+	}
+}
