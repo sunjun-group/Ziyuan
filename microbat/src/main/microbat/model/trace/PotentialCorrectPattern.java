5e8b29df2e92d7cf074a55fac141506e76f8f85f
Yun Lin <llmhyy@gmail.com>
2016-03-07 20:35:43 +0800

    do not report the path pattern exception

--- microbat/src/main/microbat/model/trace/PotentialCorrectPattern.java
+++ microbat/src/main/microbat/model/trace/PotentialCorrectPattern.java
@@ -44,7 +44,7 @@ public class PotentialCorrectPattern {
 				checkLabelInstance(path);
 				this.labelInstance = path;
 			} catch (LabelPathIncompatibleException e) {
-				e.printStackTrace();
+				//e.printStackTrace();
 			}
 		}
 		else{
