8203aaedc83f5b141dbf4f71a69e875c23e882cb
Yun Lin <llmhyy@gmail.com>
2016-03-25 10:58:47 +0800

    adjust the similarity computation for trace node: 1) same location provide a slight bonus 2) same variable name provide a bonus 3) synonymous variable with same value provide a bonus

--- microbat/src/evaluation/microbat/evaluation/util/TraceNodeSimilarityComparator.java
+++ microbat/src/evaluation/microbat/evaluation/util/TraceNodeSimilarityComparator.java
@@ -13,11 +13,15 @@ public class TraceNodeSimilarityComparator {
 
 	public double compute(TraceNode traceNode1, TraceNode traceNode2) {
 		
+		if(traceNode1.getOrder() == 41 && traceNode2.getOrder() == 38){
+			System.currentTimeMillis();
+		}
+		
 		if(traceNode1.hasSameLocation(traceNode2)){
-			int commonReadVarWithSameValue = findCommonVarWithSameValue(traceNode1.getReadVariables(), traceNode1, 
-					traceNode2.getReadVariables(), traceNode2);
-			int commonWrittenVarWithSameValue = findCommonVarWithSameValue(traceNode1.getWrittenVariables(), traceNode1,
-					traceNode2.getWrittenVariables(), traceNode2);
+			double commonReadVarWithSameValue = findCommonVarWithSameValue(traceNode1.getReadVariables(), 
+					traceNode2.getReadVariables());
+			double commonWrittenVarWithSameValue = findCommonVarWithSameValue(traceNode1.getWrittenVariables(),
+					traceNode2.getWrittenVariables());
 			
 			int totalVars = traceNode1.getReadVariables().size() + traceNode1.getWrittenVariables().size() +
 					traceNode2.getWrittenVariables().size() + traceNode2.getReadVariables().size();
@@ -29,91 +33,90 @@ public class TraceNodeSimilarityComparator {
 			else{
 				score = (2*(double)commonReadVarWithSameValue+2*commonWrittenVarWithSameValue)/totalVars;
 			}
-			return score;
+			
+			/**
+			 * give a value for same location similarity
+			 */
+			return 0.05 + 0.95*score;
 		}
 		
 		return 0;
 	}
 
-	private int findCommonVarWithSameValue(List<VarValue> variables1, TraceNode node1, List<VarValue> variables2, TraceNode node2) {
-		int common = 0;
+	private double findCommonVarWithSameValue(List<VarValue> variables1, List<VarValue> variables2) {
+		double common = 0;
 		for(VarValue var1: variables1){
 			for(VarValue var2: variables2){
-				if(isCommon(var1, node1, var2, node2)){
-					common++;
+				double commonness = findCommonness(var1, var2);
+				if(commonness > 0){
+					common += commonness;
 					break;
 				}
+//				if(isCommon(var1, var2)){
+//					common++;
+//					break;
+//				}
 			}
 		}
 		return common;
 	}
 
-	private boolean hasSameReadVars(TraceNode node1, TraceNode node2){
-		List<VarValue> var1s = node1.getReadVariables();
-		List<VarValue> var2s = node2.getReadVariables();
-		
-		boolean isExactlyTheSame = isExactlyTheSame(var1s, node1, var2s, node2);
-		return isExactlyTheSame;
-	}
+//	private boolean hasSameReadVars(TraceNode node1, TraceNode node2){
+//		List<VarValue> var1s = node1.getReadVariables();
+//		List<VarValue> var2s = node2.getReadVariables();
+//		
+//		boolean isExactlyTheSame = isExactlyTheSame(var1s, node1, var2s, node2);
+//		return isExactlyTheSame;
+//	}
 	
-	@SuppressWarnings("unchecked")
-	private boolean isExactlyTheSame(List<VarValue> var1s, TraceNode node1, List<VarValue> var2s, TraceNode node2) {
-		ArrayList<VarValue> clonedVar1s = (ArrayList<VarValue>) ((ArrayList<VarValue>)var1s).clone();
-		ArrayList<VarValue> clonedVar2s = (ArrayList<VarValue>) ((ArrayList<VarValue>)var2s).clone();
+//	@SuppressWarnings("unchecked")
+//	private boolean isExactlyTheSame(List<VarValue> var1s, TraceNode node1, List<VarValue> var2s, TraceNode node2) {
+//		ArrayList<VarValue> clonedVar1s = (ArrayList<VarValue>) ((ArrayList<VarValue>)var1s).clone();
+//		ArrayList<VarValue> clonedVar2s = (ArrayList<VarValue>) ((ArrayList<VarValue>)var2s).clone();
+//		
+//		Iterator<VarValue> iter1 = clonedVar1s.iterator();
+//		while(iter1.hasNext()){
+//			VarValue var1 = iter1.next();
+//			
+//			Iterator<VarValue> iter2 = clonedVar2s.iterator();
+//			while(iter2.hasNext()){
+//				VarValue var2 = iter2.next();
+//				
+//				if(isCommon(var1, var2)){
+//					iter1.remove();
+//					iter2.remove();
+//					break;
+//				}
+//			}
+//		}
+//		
+//		return clonedVar1s.isEmpty() && clonedVar2s.isEmpty();
+//	}
+
+	private double findCommonness(VarValue var1, VarValue var2) {
+		double commonness = 0;
 		
-		Iterator<VarValue> iter1 = clonedVar1s.iterator();
-		while(iter1.hasNext()){
-			VarValue var1 = iter1.next();
-			
-			Iterator<VarValue> iter2 = clonedVar2s.iterator();
-			while(iter2.hasNext()){
-				VarValue var2 = iter2.next();
-				
-				if(isCommon(var1, node1, var2, node2)){
-					iter1.remove();
-					iter2.remove();
-					break;
+		if(var1 instanceof VirtualValue && var2 instanceof VirtualValue){
+			if(var1.getStringValue().equals(var2.getStringValue())){
+				commonness = 1;
+			}
+		}
+		else{
+			boolean flag1 = var1.getVarName().equals(var2.getVarName());
+			if(flag1){
+				commonness += 0.5;
+				if(!(var1 instanceof ReferenceValue) && !(var2 instanceof ReferenceValue)){
+					if(var1.getStringValue().equals(var2.getStringValue())){
+						commonness += 0.5;
+					}
+				}
+				else if((var1 instanceof ReferenceValue) && (var2 instanceof ReferenceValue)){
+					commonness += 0.5;
 				}
 			}
 		}
 		
-		return clonedVar1s.isEmpty() && clonedVar2s.isEmpty();
-	}
-
-	private boolean isCommon(VarValue var1, TraceNode node1, VarValue var2, TraceNode node2) {
-		
-		if(var1 instanceof VirtualValue && var2 instanceof VirtualValue){
-			return var1.getStringValue().equals(var2.getStringValue());
-		}
-		else{
-			boolean flag = isOrdinaryCommon(var1, var2);
-			return flag;
-		}
-		
-//		return false;
-	}
-
-	private TraceNode findDominatorReturn(TraceNode node, VarValue var) {
-		for(TraceNode dataDominator: node.getDataDominator().keySet()){
-			List<String> varIDs = node.getDataDominator().get(dataDominator);
-			String varID = varIDs.get(0);
-			
-			if(var.getVarID().equals(varID)){
-				return dataDominator;
-			}
-		}
-		
-		return null;
-	}
-
-	private boolean isOrdinaryCommon(VarValue var1, VarValue var2) {
-		boolean flag1 = var1.getVarName().equals(var2.getVarName());
-		boolean flag2 = true; 
-		if(!(var1 instanceof ReferenceValue) && !(var2 instanceof ReferenceValue)){
-			flag2 = var1.getStringValue().equals(var2.getStringValue());
-		}
-		
-		return flag1 && flag2;
+		return commonness;
 	}
 
 }
