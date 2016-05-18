d54038140b5a8d583ae6d36f53b482b0c4a5e0bb
chichen <1021184207@qq.com>
2016-05-18 16:12:29 +0800

    add Fibonacci benchmark

---
+++ app/learntest/src/test/java/testdata/benchmark/Fibonacci.java
@@ -0,0 +1,36 @@
+package testdata.benchmark;
+
+public class Fibonacci{
+	
+	 public int FibonacciTest(int number){
+		 
+		if(number < 0){
+			System.out.println("Fibonacci cannot be called with an integer value less than 0");
+			return -1;
+		}
+
+         switch (number)
+         {
+             case 0:
+                 return 0;
+             case 1:
+                 return 1;
+             default:
+                 {
+                     int[] fibs = new int[number + 1];
+                     fibs[0] = 0;
+                     fibs[1] = 1;
+
+                     // populate fibs with fib sequence
+                     for (int i = 2; i <= number; i++)
+                     {
+                         fibs[i] = fibs[i - 1] + fibs[i - 2];
+                     }
+
+                     return fibs[number];
+                 }
+         }
+		 
+	 }
+
+}
