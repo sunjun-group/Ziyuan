a9629718ee107d3e8b3a60060cdc5bae40a910f2
Yun Lin <llmhyy@gmail.com>
2016-10-22 16:20:57 +0800

    change all maven dependencies into eclipse plugin dependencies

--- app/learntest/src/main/java/learntest/handler/GenerateTestHandler.java
+++ app/learntest/src/main/java/learntest/handler/GenerateTestHandler.java
@@ -4,12 +4,21 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 
+import learntest.main.TestGenerator;
+import sav.common.core.SavException;
+
 public class GenerateTestHandler extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		
-		System.out.println("ok");
+		try {
+			new TestGenerator().genTest();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SavException e) {
+			e.printStackTrace();
+		}
 		
 		return null;
 	}
