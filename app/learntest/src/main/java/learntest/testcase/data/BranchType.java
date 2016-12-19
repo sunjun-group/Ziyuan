dabf73dc55baffaf6e9fb6438e34fcaf43d09528
Yun Lin <llmhyy@gmail.com>
2016-12-19 17:36:59 +0800

    minor

--- app/learntest/src/main/java/learntest/testcase/data/BranchType.java
+++ app/learntest/src/main/java/learntest/testcase/data/BranchType.java
@@ -4,4 +4,18 @@ public class BranchType {
 	public static int FALSE = 0;
 	public static int TRUE = 1;
 	public static int MORE = 2;
+	
+	public static String getBranchType(int no){
+		if(no == 0){
+			return "false";
+		}
+		else if(no == 1){
+			return "true";
+		}
+		else if(no == 2){
+			return "more";
+		}
+		
+		return null;
+	}
 }
