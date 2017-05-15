03852d62294eefd2d3a9ed7cefba5ea798fbd8f6
Yun Lin <llmhyy@gmail.com>
2017-05-15 14:23:41 +0800

    retrieve class path of target project.

--- jdart/src/main/jdart/handler/RunJDartHandler.java
+++ jdart/src/main/jdart/handler/RunJDartHandler.java
@@ -1,21 +1,37 @@
 package jdart.handler;
 
+import java.util.List;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.runtime.CoreException;
 
+import learntest.util.LearnTestUtil;
 import main.RunJPF;
+import sav.common.core.SystemVariables;
+import sav.commons.TestConfiguration;
+import sav.strategies.dto.AppJavaClassPath;
 
 public class RunJDartHandler extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		
+		List<String> classpaths = LearnTestUtil.getPrjectClasspath();
+		StringBuffer buffer = new StringBuffer();
+		for(String classpath: classpaths){
+			buffer.append(classpath);
+			buffer.append(";");
+		}
+		String pathString = buffer.toString();
+		
 		String[] quicksortConfig = new String[]{
 				"+app=libs/jdart/jpf.properties",
 				"+site=libs/jpf.properties",
-				"+jpf-jdart.classpath+=../../bin",
-				"+target=Sorting",
+//				"+jpf-jdart.classpath+=../../bin",
+				"+jpf-jdart.classpath+=" + pathString,
+				"+target=com.Sorting",
 				"+concolic.method=quicksort",
 				"+concolic.method.quicksort=${target}.quicksort(a:int[])",
 				"+concolic.method.quicksort.config=all_fields_symbolic"
@@ -23,9 +39,22 @@ public class RunJDartHandler extends AbstractHandler {
 		
 		RunJPF.run(quicksortConfig);
 		
-		System.currentTimeMillis();
 		
 		return null;
+	}
+	
+	private AppJavaClassPath initAppJavaClassPath() throws CoreException {
+//		IProject project = IProjectUtils.getProject(LearnTestConfig.projectName);
+//		IJavaProject javaProject = IProjectUtils.getJavaProject(project);
+		AppJavaClassPath appClasspath = new AppJavaClassPath();
+		appClasspath.setJavaHome(TestConfiguration.getJavaHome());
+//		appClasspath.setJavaHome(IProjectUtils.getJavaHome(javaProject));
+		appClasspath.addClasspaths(LearnTestUtil.getPrjectClasspath());
+		String outputPath = LearnTestUtil.getOutputPath();
+		appClasspath.setTarget(outputPath);
+		appClasspath.setTestTarget(outputPath);
+		appClasspath.getPreferences().set(SystemVariables.PROJECT_CLASSLOADER, LearnTestUtil.getPrjClassLoader());
+		return appClasspath;
 	}
 
 }
