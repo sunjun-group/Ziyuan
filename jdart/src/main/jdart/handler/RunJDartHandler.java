b4baa201d9a4ba159bd399ea0a6481676aa8f70a
Yun Lin <llmhyy@gmail.com>
2017-06-01 08:08:13 +0800

    write some code for configuring jdart parameters

--- jdart/src/main/jdart/handler/RunJDartHandler.java
+++ jdart/src/main/jdart/handler/RunJDartHandler.java
@@ -6,7 +6,12 @@ import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.SingleVariableDeclaration;
 
+import learntest.main.LearnTestConfig;
 import learntest.util.LearnTestUtil;
 import main.RunJPF;
 import sav.common.core.SystemVariables;
@@ -15,6 +20,43 @@ import sav.strategies.dto.AppJavaClassPath;
 
 public class RunJDartHandler extends AbstractHandler {
 
+	class MethodFinder extends ASTVisitor{
+		
+		MethodDeclaration method;
+		
+		String methodName;
+		int lineNumber;
+		CompilationUnit cu;
+		
+		public MethodFinder(String methodName, int lineNumber, CompilationUnit cu) {
+			super();
+			this.methodName = methodName;
+			this.lineNumber = lineNumber;
+			this.cu = cu;
+		}
+
+		public boolean visit(MethodDeclaration md){
+			
+			if(this.method!=null){
+				return false;
+			}
+			
+			if(md.getName().getFullyQualifiedName().equals(methodName)){
+				int startLine = cu.getLineNumber(md.getStartPosition());
+				int endLine = cu.getLineNumber(md.getStartPosition()+md.getLength());
+				
+				if(startLine<=lineNumber && lineNumber<=endLine){
+					this.method = md;
+					return false;
+				}
+				
+			}
+			
+			return true;
+			
+		}
+	}
+	
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		
@@ -26,13 +68,41 @@ public class RunJDartHandler extends AbstractHandler {
 		}
 		String pathString = buffer.toString();
 		
+		String className = LearnTestConfig.targetClassName;
+		String methodName = LearnTestConfig.targetMethodName;
+		String lineNumber = LearnTestConfig.targetMethodLineNum;
+		CompilationUnit cu = LearnTestUtil.findCompilationUnitInProject(className);
+		
+		MethodFinder finder = new MethodFinder(methodName, Integer.valueOf(lineNumber), cu);
+		cu.accept(finder);
+		MethodDeclaration md = finder.method;
+		
+		StringBuffer buffer2 = new StringBuffer();
+		buffer2.append("(");
+		for(Object obj: md.parameters()){
+			if(obj instanceof SingleVariableDeclaration){
+				SingleVariableDeclaration svd = (SingleVariableDeclaration)obj;
+				String paramName = svd.getName().getIdentifier();
+				String paramType = svd.getType().toString();
+				
+				buffer2.append(paramName);
+				buffer2.append(":");
+				buffer2.append(paramType);
+				
+				buffer2.append(",");
+			}
+		}
+		String paramString = buffer2.toString();
+		paramString = paramString.substring(0, paramString.length()-1);
+		paramString = paramString + ")";
+		
 		String[] quicksortConfig = new String[]{
 				"+app=libs/jdart/jpf.properties",
 				"+site=libs/jpf.properties",
 				"+jpf-jdart.classpath+=" + pathString,
-				"+target=com.Sorting",
-				"+concolic.method=quicksort",
-				"+concolic.method.quicksort=${target}.quicksort(a:int[])",
+				"+target=" + className,
+				"+concolic.method=" + methodName,
+				"+concolic.method.quicksort=${target}." + methodName + paramString,
 				"+concolic.method.quicksort.config=all_fields_symbolic"
 		};
 		
