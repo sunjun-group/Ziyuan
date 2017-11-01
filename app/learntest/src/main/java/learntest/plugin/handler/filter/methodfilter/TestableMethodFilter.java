28f1b48b7920269c78e2f0442bcebdf17e612209
zhanghr <1044235759@qq.com>
2017-11-01 15:16:34 +0800

    #117 discard those methods which none of its input variables (including parameters and read fields) is of primitive type or primitive array type

--- app/learntest/src/main/java/learntest/plugin/handler/filter/methodfilter/TestableMethodFilter.java
+++ app/learntest/src/main/java/learntest/plugin/handler/filter/methodfilter/TestableMethodFilter.java
@@ -10,12 +10,18 @@ package learntest.plugin.handler.filter.methodfilter;
 
 import java.util.List;
 
+import org.eclipse.jdt.core.IField;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.ITypeRoot;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.core.Signature;
 import org.eclipse.jdt.core.dom.ArrayType;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
 import org.eclipse.jdt.core.dom.Modifier;
 import org.eclipse.jdt.core.dom.SingleVariableDeclaration;
 import org.eclipse.jdt.core.dom.Type;
+import org.eclipse.jdt.internal.core.SourceType;
 
 import sav.common.core.utils.CollectionUtils;
 import sav.common.core.utils.PrimitiveUtils;
@@ -29,7 +35,9 @@ public class TestableMethodFilter implements IMethodFilter {
 	@Override
 	public boolean isValid(CompilationUnit cu, MethodDeclaration md) {
 		if(md.isConstructor() || md.parameters().isEmpty()
-				|| !Modifier.isPublic(md.getModifiers()) || Modifier.isAbstract(md.getModifiers())){
+				|| !Modifier.isPublic(md.getModifiers()) || Modifier.isAbstract(md.getModifiers())
+						|| !containsAtLeastOnePrimitiveTypeParam(md.parameters())
+						|| !containsAtLeastOnePrimitiveTypeField(cu)){
 			return false;
 		}
 		if (CollectionUtils.isEmpty(md.getBody().statements())) {
@@ -40,7 +48,7 @@ public class TestableMethodFilter implements IMethodFilter {
 	
 	/* backup from previous implementation */
 	@SuppressWarnings("unused")
-	private boolean containsAtLeastOnePrimitiveType(List<?> parameters){
+	private boolean containsAtLeastOnePrimitiveTypeParam(List<?> parameters){
 		for (Object obj : parameters) {
 			if (obj instanceof SingleVariableDeclaration) {
 				SingleVariableDeclaration svd = (SingleVariableDeclaration) obj;
@@ -55,6 +63,55 @@ public class TestableMethodFilter implements IMethodFilter {
 					}
 				}
 			}
+		}
+		return false;
+	}
+	
+	@SuppressWarnings("restriction")
+	private boolean containsAtLeastOnePrimitiveTypeField(CompilationUnit cu){
+		ITypeRoot type = cu.getTypeRoot();
+		try {
+			IJavaElement[] elements = type.getChildren();
+			for (int i = 0; i < elements.length; i++) {
+				IJavaElement element = elements[i];
+				element.getElementName();
+				if (element instanceof SourceType) {
+					SourceType source = (SourceType) element;
+					IField[] fields = source.getFields();
+					for (IField iField : fields) {
+						String fieldT = iField.getTypeSignature();
+						if (isPrimitiveType(fieldT)) {
+							return true;
+						}
+					}
+				}
+			}
+		} catch (JavaModelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	private boolean isPrimitiveType(String signature) {
+		if (signature.equals(Signature.SIG_INT)
+				|| signature.equals(Signature.SIG_SHORT) 
+				|| signature.equals(Signature.SIG_BYTE)
+				|| signature.equals(Signature.SIG_BOOLEAN) 
+				|| signature.equals(Signature.SIG_CHAR) 
+				|| signature.equals(Signature.SIG_DOUBLE)
+				|| signature.equals(Signature.SIG_FLOAT)
+				|| signature.equals(Signature.SIG_LONG) ) {
+			return true;
+		}else if (signature.equals("[" + Signature.SIG_INT)
+				|| signature.equals("[" + Signature.SIG_SHORT) 
+				|| signature.equals("[" + Signature.SIG_BYTE)
+				|| signature.equals("[" + Signature.SIG_BOOLEAN) 
+				|| signature.equals("[" + Signature.SIG_CHAR) 
+				|| signature.equals("[" + Signature.SIG_DOUBLE)
+				|| signature.equals("[" + Signature.SIG_FLOAT)
+				|| signature.equals("[" + Signature.SIG_LONG)) {
+			
 		}
 		return false;
 	}
