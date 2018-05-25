e6e51857777181e9597ff98f5b00d6ce49bf3681
lylytran <lylypp6987@gmail.com>
2018-05-25 15:27:51 +0800

    avoid to config manually

--- jdart/src/main/config/PathConfiguration.java
+++ jdart/src/main/config/PathConfiguration.java
@@ -1,6 +1,23 @@
 package config;
 
+import java.io.File;
+
 public class PathConfiguration {
 	public static String jdartRoot = "E:\\git\\Ziyuan\\jdart";
 	public static String savRoot = "E:\\git\\Ziyuan\\app\\sav.commons";
+	
+	static {
+		if (!new File(jdartRoot).exists()) {
+			jdartRoot = getProjectBaseDir() + "/jdart";
+			savRoot = getProjectBaseDir() + "/app/sav.commons";
+		}
+	}
+	
+	public static String getProjectBaseDir() {
+		String path = new File(PathConfiguration.class.getProtectionDomain().getCodeSource().getLocation().getPath())
+				.getAbsolutePath();
+		path = path.replace("\\", "/");
+		path = path.substring(0, path.indexOf("/jdart"));
+		return path;
+	}
 }
