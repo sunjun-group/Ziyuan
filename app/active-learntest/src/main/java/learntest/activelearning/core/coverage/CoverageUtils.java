1b51481dd6fa1a606247896d0705155bf6e95306
Tran Thi Ly Ly <lylypp6987@gmail.com>
2018-08-05 17:53:13 +0800

    correct branch coverage counting

--- app/active-learntest/src/main/java/learntest/activelearning/core/coverage/CoverageUtils.java
+++ app/active-learntest/src/main/java/learntest/activelearning/core/coverage/CoverageUtils.java
@@ -1,20 +1,29 @@
 package learntest.activelearning.core.coverage;
 
 import microbat.instrumentation.cfgcoverage.graph.CoverageSFlowGraph;
+import sav.common.core.utils.CollectionUtils;
 import microbat.instrumentation.cfgcoverage.graph.CoverageSFNode;
 
 
 public class CoverageUtils {
 
 	public static double getBranchCoverage(CoverageSFlowGraph coverageSFlowGraph, String methodId) {
-	  double allBranches = 0.00001;
+	  int allBranches = 0;
 	  int coveredBranches = 0;
+	  System.out.println();
 	  for(CoverageSFNode node : coverageSFlowGraph.getNodeList()) {
-		  if(node.getBranches().isEmpty()) continue;
-		  for(CoverageSFNode branchNode : node.getBranches()) {
-			  allBranches ++;
-			  if(!(branchNode.getCoveredTestcases().isEmpty())) coveredBranches ++;
-		  }
+			if (!node.isConditionalNode()) {
+				continue;
+			}
+			for (CoverageSFNode branchNode : node.getBranches()) {
+				allBranches++;
+				if (!CollectionUtils.isEmpty(node.getCoveredTestcasesOnBranches().get(branchNode))) {
+					coveredBranches++;
+				}
+			}
+	  }
+	  if (allBranches == 0) {
+		  return 1;
 	  }
 	  double coverage = coveredBranches / (double)allBranches;
 	  return coverage;
